598972a4acb155cecea23b9e7a5754d6910486a1
CETECOM <cetecom_conce@duoc.cl>
2024-05-28 11:41:17 -0400

    wa

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index,name='index')
+    path('', views.index,name='index'),
+    path('productos/',views.productos, name='productos')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "core/index.html")
+
+def productos(request):
+    return render(request, "core/productos.html")
